d8dcc62d9c5708d14feba1a2480ded746cad3229
Gonzalo <gonzalo.penia1998@gmail.com>
2025-05-26 19:28:07 -0300

    primer commit maps-app

---
+++ src/MapsApp.tsx
@@ -0,0 +1,11 @@
+import { PlacesProvider } from "./context";
+
+import React from 'react'
+
+export const MapsApp = () => {
+  return (
+    <PlacesProvider>
+        <h1>Hola mundo, denuevo</h1>
+    </PlacesProvider>
+)
+}
